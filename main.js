d19557cb00791b37810fa6031a3af2cac9d34006
petestingforyou <109937397+petestingforyou@users.noreply.github.com>
2024-04-29 02:39:26 -0600

    Actualizar main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 var arrayData=new array();
 var archivoTxt=newXMLHTTPRequest();
 var fileRuta=datos.txt
+var dataSum=0;
+
+archivoTxt.open("Get",fileRuta,false);
+archivoTxt.send(null);
+var txt=archivoTxt.responseText;
+
+for(var i=0; i<txt.length;i++){
+arrayData.push(txt[i]);
+}
+arrayData.foreach(function(data){
+console.log(data);
+dataSum+=parseInt(data);
+});
+
+if (dataSum==0){
+console.log ('inserte dato');
