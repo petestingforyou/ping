8d70395dffd0bea822d8bb872196519aa7c95093
petestingforyou <109937397+petestingforyou@users.noreply.github.com>
2026-07-04 13:58:16 -0600

    Add camera initialization function in app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+const video = document.getElementById("video");
 
+async function iniciarCamara() {
+
+    try {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true
+        });
+
+        video.srcObject = stream;
+
+        await video.play();
+
+        console.log("Cámara iniciada");
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("No fue posible acceder a la cámara.");
+
+    }
+
+}
+
+iniciarCamara();
